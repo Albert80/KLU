e7510a45a0432e6d1eb19c6fe38591ae34ad35f9
Cesar Trejo <cesaratj27@gmail.com>
2025-04-13 22:55:38 -0600

    Create FastApi app

---
+++ klu_backend/main.py
@@ -0,0 +1,31 @@
+from app.api.api import api_router
+from app.core.config import settings
+from app.db.session import engine
+from app.models.transaction import Base
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# Crear tablas en la base de datos
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cambiar en producción
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Payments App API"}
