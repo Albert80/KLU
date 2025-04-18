0263119914abf0db83de8bdfc8df5d1c8528eba5
Cesar Trejo <cesaratj27@gmail.com>
2025-04-18 03:44:16 -0600

    Fix NGINX config

--- klu_backend/main.py
+++ klu_backend/main.py
@@ -4,18 +4,16 @@ from app.db.session import engine
 from app.models.transaction import Base
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 # Crear tablas en la base de datos (pero ya existe alembic)
 # Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
+    docs_url="/docs",
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     debug=True,
 )
-
-app.add_middleware(HTTPSRedirectMiddleware)
 
 # Configurar CORS
 app.add_middleware(
